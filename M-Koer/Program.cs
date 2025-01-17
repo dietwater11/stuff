786631e656596cb78211c048f9ac9beb7f27d57c
dietwater11 <rico.toomesoo@gmail.com>
2025-01-17 09:46:33 +0200

    Add files via upload

---
+++ M-Koer/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace M_Koer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta esimene arv");
+            string number1 = Console.ReadLine();
+            int nr1 = int.Parse(number1);
+
+            Console.WriteLine("Sisesta teine arv");
+            string number2 = Console.ReadLine();
+            int nr2 = int.Parse(number2);
+
+            Console.WriteLine("Sisesta tehe ( +, -, *, / )");
+            string mark = Console.ReadLine();
+
+            if (mark == "+")
+            {
+                Console.WriteLine(nr1 + nr2);
+            }
+            else if (mark == "-")
+            {
+                Console.WriteLine(nr1 - nr2);
+            }
+            else if (mark == "*")
+            {
+                Console.WriteLine(nr1 * nr2);
+            }
+            else
+            {
+                Console.WriteLine(nr1 / nr2);
+            }
+        }
+    }
+}
